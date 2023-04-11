3b7245d916a3d450c31d43ebb200a72c5bf15e58
Pedro Silveira <silveirapedro2003@gmail.com>
2023-04-11 13:41:32 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/pedrosilveira/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pedrosilveira.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pedrosilveira.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+	
+}
